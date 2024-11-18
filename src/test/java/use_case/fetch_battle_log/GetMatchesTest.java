898b93d83ad8bbb97d3eba6aea7cac4a0d1a61d2
harrypenguin <pengzehao2005@gmail.com>
2024-11-17 19:44:38 -0500

    Updated tests

--- src/test/java/use_case/fetch_battle_log/GetMatchesTest.java
+++ src/test/java/use_case/fetch_battle_log/GetMatchesTest.java
@@ -1,6 +1,7 @@
 package use_case.fetch_battle_log;
 
 import data_access.APIDataAccessObject;
+import entity.ClubFactory;
 import entity.MatchFactory;
 import entity.UserFactory;
 import org.junit.Test;
@@ -9,7 +10,7 @@ public class GetMatchesTest {
 
     @Test
     public void getMatchesTest() {
-        APIDataAccessObject api = new APIDataAccessObject(new UserFactory(), new MatchFactory());
+        APIDataAccessObject api = new APIDataAccessObject(new UserFactory(), new MatchFactory(), new ClubFactory());
         api.getMatches("#99uv0990");
     }
 
--- src/test/java/use_case/fetch_user/GetUserTest.java
+++ src/test/java/use_case/fetch_user/GetUserTest.java
@@ -1,5 +1,6 @@
 package use_case.fetch_user;
 
+import entity.ClubFactory;
 import org.junit.Test;
 
 import data_access.APIDataAccessObject;
@@ -9,7 +10,7 @@ public class GetUserTest {
 
 	@Test
 	public void getUserSuccessTest() {
-		APIDataAccessObject api = new APIDataAccessObject(new UserFactory(), new MatchFactory());
+		APIDataAccessObject api = new APIDataAccessObject(new UserFactory(), new MatchFactory(), new ClubFactory());
 		api.getUser("#822GY0JYY");
 	}
 
