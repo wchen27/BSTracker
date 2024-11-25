5e5e565ab4d834beccffa0a76489e96959233310
harrypenguin <pengzehao2005@gmail.com>
2024-11-24 19:16:49 -0500

    Added test for GetClub use case

--- src/test/java/use_case/fetch_club/GetClubTest.java
+++ src/test/java/use_case/fetch_club/GetClubTest.java
@@ -3,15 +3,24 @@ package use_case.fetch_club;
 import data_access.APIDataAccessObject;
 import entity.ClubFactory;
 import entity.MatchFactory;
+import entity.User;
 import entity.UserFactory;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GetClubTest {
 
     @Test
     public void getClubSuccessTest() {
         APIDataAccessObject api = new APIDataAccessObject(new UserFactory(), new MatchFactory(), new ClubFactory());
-        api.getUser("#2VOQL2LUG");
+        List<User> members = api.getMembers("#2VOQL2LUG");
+        List<String> membernames = new ArrayList<>();
+        for (User member : members) {
+            membernames.add(member.getUsername());
+        }
+        assert(membernames.contains("Thigamore"));
     }
 
 }
