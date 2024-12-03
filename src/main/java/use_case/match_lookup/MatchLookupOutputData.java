64fc1a8cf54b9e909abb018b20ff4572b1df4f06
wchen27 <wilson0109@outlook.com>
2024-12-03 12:01:22 -0500

    finished tests for MatchView

--- src/main/java/use_case/match_lookup/MatchLookupOutputData.java
+++ src/main/java/use_case/match_lookup/MatchLookupOutputData.java
@@ -7,29 +7,13 @@ import java.util.List;
 public class MatchLookupOutputData {
 
     private final List<Match> matches;
-    private double winrate;
 
     public MatchLookupOutputData(List<Match> matches) {
         this.matches = matches;
-        int numWins = 0;
-        if (matches.isEmpty()) {
-            this.winrate = 0;
-        }
-        else {
-            for (Match match : matches) {
-                if (match.isVictory()) {
-                    numWins++;
-                }
-            }
-            this.winrate = numWins / (double) matches.size();
-        }
     }
 
     public List<Match> getMatches() {
         return matches;
     }
 
-    public double getWinrate() {
-        return winrate;
-    }
 }
--- src/test/java/use_case/fetch_battle_log/GetMatchesTest.java
+++ src/test/java/use_case/fetch_battle_log/GetMatchesTest.java
@@ -3,20 +3,58 @@ package use_case.fetch_battle_log;
 import data_access.APIDataAccessObject;
 import data_access.FileDataAccessObject;
 import entity.ClubFactory;
+import entity.Match;
 import entity.MatchFactory;
 import entity.UserFactory;
 import io.github.cdimascio.dotenv.Dotenv;
 
+import org.json.JSONException;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
 
 public class GetMatchesTest {
 
     @Test
+    // tests
     public void getMatchesTest() {
         Dotenv env = Dotenv.load();
         APIDataAccessObject api = new APIDataAccessObject(new UserFactory(), new MatchFactory(), new ClubFactory(),
                 env);
-        api.getMatches("#99uv0990");
+        List<Match> response = api.getMatches("#99uv0990");
+        assertNotEquals(response.size(), 0);
+    }
+
+    @Test(expected = JSONException.class)
+    // tests failing to fetch a match list
+    public void getMatchesFailedTest() {
+        Dotenv env = Dotenv.load();
+        APIDataAccessObject api = new APIDataAccessObject(new UserFactory(), new MatchFactory(), new ClubFactory(),
+                env);
+        api.getMatches("fail");
+    }
+
+    @Test
+    // tests the validity of match data
+    public void getMatchesDataTest() {
+        Dotenv env = Dotenv.load();
+        APIDataAccessObject api = new APIDataAccessObject(new UserFactory(), new MatchFactory(), new ClubFactory(),
+                env);
+        List<Match> response = api.getMatches("#99uv0990");
+        Match match = response.get(0);
+        assertNotNull(match);
+        assertNotNull(match.getMap());
+        assertNotNull(match.getMode());
+        assertNotNull(match.getStarPlayer());
+        assertNotNull(match.getTime());
+        assertNotNull(match.getMode());
+        match.getTrophyChange();
+        match.getTrophyCount();
+        match.isVictory();
+        match.toString();
     }
 
 }
